ea42fe2f9ef1f464a5b5f00f9bb15bd12d837600
NicolasParadaA <nicolas.parada.arias@gmail.com>
2026-03-04 15:35:00 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+      <nav class="d-flex justify-content-center py-3">
+        <RouterLink to="/" class="mx-2">Home</RouterLink>
+        <RouterLink to="/about" class="mx-2">About</RouterLink>
+      </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <header>
+    <h1 class="text-center">Nuestros productos</h1>
+  </header>
+
+  <main class="container">
+    <section>
+      <h2>Todos los productos disponibles...</h2>
+      <div class="row justify-content-evenly row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4" v-if="products.length">
+        <div class="col" v-for="product in products" :key="product.id">
+          <CardProduct :product/>
+        </div>
+      </div>
+    </section>
+  </main>
+
+</div>
+</template>
+
+<script setup>
+import CardProduct from '@/components/CardProduct.vue';
+import useFetch from '@/composables/fetch.js';
+import { ref, onMounted } from 'vue';
+
+const products = ref([])
+
+onMounted(async () => {
+  let url = "https://dummyjson.com/products"
+  let {data, error} = await useFetch(url)
+  
+  if(error.value) return alert("Error al intentar conectarse a la API...")
+
+  //DESDE AQUI MANIPULAMOS LOS DATOS
+  let listProducts = data.value.products
+  
+  products.value = listProducts.map(p => {
+    let {id, title, description, brand, price, sku, thumbnail, images} = p
+    let objProduct = {id, title, description, brand, price, sku, thumbnail, images}
+    return objProduct
+  })
+
+})
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
